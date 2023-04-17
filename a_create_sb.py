bb35412ea6c29cc27d20061340a00a092bd12378
Mahboob-Alam <iammahboob.a@gmail.com>
2023-04-17 11:25:54 +0530

    create database

---
+++ a_create_sb.py
@@ -0,0 +1,58 @@
+
+
+"""
+Conceptual Session : 4 - 1 -- Bank Project Using Python and SQL 
+Moudle Date : 230323, Thursday (1st roja begin day)
+Watch Date : 170423, Monday, 25th Roja, 10.30 am 
+
+"""
+
+
+import pymysql
+
+conn = pymysql.connect(
+    
+        host = "localhost", 
+        user = "root",
+        passwd = "learnlearn"
+
+)
+
+
+
+mycursor = conn.cursor()
+
+sql_command =           """
+
+                                        CREATE DATABASE BankProject;
+
+                                     """
+
+
+mycursor.execute(sql_command)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
